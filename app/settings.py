be949efe1cafc485448c97c9ed14158b110921a7
yaakovLowenstein <lowensteinyaakov@gmail.com>
2025-03-13 04:03:42 -0700

    Update Prod settings

--- app/settings.py
+++ app/settings.py
@@ -85,8 +85,12 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_NAME"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": 5432,
     }
 }
 
